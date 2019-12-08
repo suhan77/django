e1a51c3e5b4be39c9434219c735ed1a71dadce2a
suhan <suahn320@gmail.com>
2019-12-08 15:26:38 +0900

    post parameter

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import numpy as np
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    vals = np.array([1,2,3,4])
+    return render(request, 'blog/post_list.html', {'vals' : vals})
